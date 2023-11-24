7fd6158c1a304c62df0e6e89ebd01c7bca940302
Ben Zen <benediktsplintergfx@gmail.com>
2023-11-24 13:55:02 +0100

    Added more questions and some fixes

--- quiz.js
+++ quiz.js
@@ -1,196 +1,104 @@
 const fragen = [
     {
-        "frage": "Ein Compiler in Python ist ein Programm, das dazu dient (Wählen Sie zwei Antworten):",
+        "frage": "A compiler in Python is a program designed to (Select two answers):",
         "optionen": [
-            "A: den Quellcode neu zu ordnen, um ihn klarer zu machen",
-            "B: den Quellcode zu überprüfen, um zu sehen, ob er korrekt ist",
-            "C: den Quellcode auszuführen",
-            "D: den Quellcode in Maschinencode zu übersetzen"
+            "A: rearrange the source code to make it clearer",
+            "B: check the source code in order to see if it's correct",
+            "C: execute the source code",
+            "D: translate the source code into machine code"
         ],
         "korrekte_antwort": ["B", "D"]
     },
     {
-        "frage": "Welche der folgenden Aussagen sind wahr? (Wählen Sie zwei Antworten):",
+        "frage": "Which of the following statements are true? (Select two answers):",
         "optionen": [
-            "A: Python-Strings sind eigentlich Listen",
-            "B: Python-Strings können verkettet werden",
-            "C: Python-Strings können wie Listen geschnitten werden",
-            "D: Python-Strings sind veränderlich"
+            "A: Python strings are actually lists",
+            "B: Python strings can be concatenated",
+            "C: Python strings can be sliced like lists",
+            "D: Python strings are mutable"
         ],
         "korrekte_antwort": ["B", "C"]
     },
     {
-        "frage": "Angenommen, dass 'String' sechs oder mehr Buchstaben lang ist, ist der folgende Slice `string[1:-2]` kürzer als der ursprüngliche String um:",
-        "optionen": ["A: vier Zeichen", "B: drei Zeichen", "C: ein Zeichen", "D: zwei Zeichen"],
+        "frage": "Assuming that String is six or more letters long the following slice string[1:-2] is shorter than the original string by:",
+        "optionen": [
+            "A: four chars",
+            "B: three chars",
+            "C: one char",
+            "D: two chars"
+        ],
         "korrekte_antwort": "D"
     },
     {
-        "frage": "Was ist die erwartete Ausgabe des folgenden Snippets?\n\ns = 'abc'\nfor i in len(s):\n    s[i] = s[i].upper()\nprint(s)",
-        "optionen": ["A: abc", "B: Der Code verursacht eine Laufzeit-Ausnahme", "C: ABC", "D: 123"],
+        "frage": "What is the expected output of the following snippet?\n\ns = 'abc'\nfor i in len(s):\n    s[i] = s[i].upper()\nprint(s)",
+        "optionen": [
+            "A: abc",
+            "B: The code will cause a runtime exception",
+            "C: ABC",
+            "D: 123"
+        ],
         "korrekte_antwort": "B"
     },
     {
-        "frage": "Kann ein Modul wie regulärer Code ausgeführt werden?",
+        "frage": "Can a module run like regular code?",
         "optionen": [
-            "A: ja, und es kann sein Verhalten zwischen dem regulären Start und dem Import unterscheiden",
-            "B: es hängt von der Python-Version ab",
-            "C: ja, aber es kann sein Verhalten zwischen dem regulären Start und dem Import nicht unterscheiden",
-            "D: nein, das ist nicht möglich; ein Modul kann importiert, aber nicht ausgeführt werden"
+            "A: yes and it can differentiate its behavior between the regular launch and import",
+            "B: it depends on the Python version",
+            "C: yes but it cannot differentiate its behavior between the regular launch and import",
+            "D: no it is not possible; a module can be imported not run"
         ],
         "korrekte_antwort": "A"
     },
     {
-        "frage": "Wählen Sie die gültigen fun() Aufrufe aus: (Wählen Sie zwei Antworten):",
+        "frage": "Select the valid fun() invocations: (Select two answers)\n\ndef fun(a, b = 0):\n    return a * b",
         "optionen": [
             "A: fun(b = 1)",
             "B: fun(a = 0)",
-            "C: fun(b = 1 0)",
+            "C: fun(b = 1, 0)",
             "D: fun(1)"
         ],
-        "korrekte_antwort": ["B", "D"]
+        "korrekte_antwort": ["A", "B"]
     },
     {
-        "frage": "Was sagt ein Dateiname wie dieser unten über die Datei aus? services.cpython-36.pyc (Wählen Sie drei Antworten):",
+        "frage": "A file name like this one below says that: services.cpython-36.pyc (Select three answers)",
         "optionen": [
-            "A: der Interpreter, der zum Generieren der Datei verwendet wurde, ist Version 3.6",
-            "B: sie wurde von CPython produziert",
-            "C: es ist die 36. Version der Datei",
-            "D: die Datei stammt aus der Quelldatei services.py"
+            "A: the interpreter used to generate the file is version 3.6",
+            "B: it has been produced by CPython",
+            "C: it is the 36th version of the file",
+            "D: the file comes from the services.py source file"
         ],
         "korrekte_antwort": ["A", "B", "D"]
     },
     {
-        "frage": "Was ist das erwartete Verhalten des folgenden Snippets?",
+        "frage": "What is the expected behavior of the following snippet?\n\ndef a(l, I):\n    return l[I]\nprint(a(0, [1))",
         "optionen": [
-            "A: verursacht eine Ausnahme",
-            "B: druckt 1",
-            "C: druckt 0 [1]",
-            "D: druckt [1]"
+            "A: cause an exception",
+            "B: print 1",
+            "C: print 0 [1]",
+            "D: print [1]"
         ],
-        "korrekte_antwort": ["A"]
+        "korrekte_antwort": "A"
     },
     {
-        "frage": "Was können Sie tun, wenn Sie einen langen Paketpfad wie diesen nicht mögen?",
+        "frage": "What can you do if you don’t like a long package path like this one?\n\nimport alpha.beta.gamma.delta.epsilon.zeta",
         "optionen": [
-            "A: Sie können einen Alias für den Namen mit dem Schlüsselwort alias erstellen",
-            "B: nichts, Sie müssen sich damit abfinden",
-            "C: Sie können ihn auf alpha.zeta verkürzen und Python wird die richtige Verbindung finden",
-            "D: Sie können einen Alias für den Namen mit dem Schlüsselwort as erstellen"
+            "A: you can make an alias for the name using the alias keyword",
+            "B: nothing you need to come to terms with it",
+            "C: you can shorten it to alpha.zeta and Python will find the proper connection",
+            "D: you can make an alias for the name using the as keyword"
         ],
-        "korrekte_antwort": ["D"]
+        "korrekte_antwort": "D"
     },
     {
-        "frage": "Was ist die erwartete Ausgabe des folgenden Codes?",
+        "frage": "What is the expected output of the following code?\n\nstr = 'abcdef'\ndef fun(s):\n    del s[2]\n    return s\n\nprint(fun(str))",
         "optionen": [
             "A: abcef",
-            "B: Das Programm verursacht eine Laufzeit-Ausnahme/Fehler",
+            "B: The program will cause a runtime exception/error",
             "C: acdef",
             "D: abdef"
         ],
-        "korrekte_antwort": ["B"]
-    },
-    {
-        "frage": "Welche der folgenden Ausdrücke werden True bewerten? (Wählen Sie zwei Antworten):",
-        "optionen": [
-            "A: 99 > '99'",
-            "B: 'ABc' > 'CAB'",
-            "C: len( \"\"\"\") <= 1",
-            "D: 'True' != True"
-        ],
-        "korrekte_antwort": ["C", "D"]
-    },
-    {
-        "frage": "Welches ist die Ausgabe des folgenden Snippets?",
-        "optionen": [
-            "A: Es wird 1 ausgegeben",
-            "B: Es wird 2 ausgegeben",
-            "C: Es wird 3 ausgegeben",
-            "D: Es tritt ein Laufzeitfehler auf"
-        ],
-        "korrekte_antwort": ["A"]
-    },
-    {
-        "frage": "Welche der folgenden Aussagen über objektorientierte Programmierung (OOP) in Python sind wahr? (Wählen Sie zwei Antworten):",
-        "optionen": [
-            "A: Klassenvariablen werden nicht in einem Objekt's __dict__ angezeigt",
-            "B: Beim Verwenden der hasattr-Methode muss der Name der Eigenschaft eine Klasse oder ein Objekt sein",
-            "C: Python erreicht die Kapselung privater Variablen durch Namensmangling",
-            "D: Eine einzelne Vererbung ist immer schwerer zu pflegen als eine multiple Vererbung"
-        ],
-        "korrekte_antwort": ["A", "C"]
-    },
-    {
-        "frage": "Welches ist die Ausgabe des folgenden Snippets?",
-        "optionen": [
-            "A: [3 2]",
-            "B: [1 2]",
-            "C: 1",
-            "D: 3"
-        ],
-        "korrekte_antwort": ["C"]
-    },
-    {
-        "frage": "Welche der folgenden Code-Snippets würden Sie einfügen, um Klasse B den Konstruktor von Klasse A aufrufen zu lassen? (Wählen Sie zwei Antworten):",
-        "optionen": [
-            "A: super().__init__()",
-            "B: super().__init__(self)",
-            "C: A.__init__()",
-            "D: A.__init__(self)"
-        ],
-        "korrekte_antwort": ["A", "D"]
-    },
-    {
-        "frage": "Welches ist die Ausgabe des folgenden Snippets?",
-        "optionen": [
-            "A: False True False",
-            "B: True False False",
-            "C: True True False",
-            "D: True True True"
-        ],
-        "korrekte_antwort": ["C"]
-    },
-    {
-        "frage": "Was ist wahr über Python-Klassenkonstruktoren? (Wählen Sie zwei Antworten):",
-        "optionen": [
-            "A: Übergeordnete Konstruktoren werden implizit während der Konstruktor-Ausführung aufgerufen",
-            "B: Der Konstruktor kann direkt aufgerufen werden",
-            "C: Der erste Parameter jeder Methode wird verwendet, um auf die Eigenschaften des Objekts zuzugreifen oder seine Methoden aufzurufen",
-            "D: Der Konstruktor kann nichts zurückgeben"
-        ],
-        "korrekte_antwort": ["A", "D"]
-    },
-    {
-        "frage": "Welches ist die Ausgabe des folgenden Snippets?",
-        "optionen": [
-            "A: 1",
-            "B: 2",
-            "C: 3",
-            "D: Der Code ist fehlerhaft"
-        ],
-        "korrekte_antwort": ["C"]
-    },
-    {
-        "frage": "Welches ist die Ausgabe des folgenden Snippets?",
-        "optionen": [
-            "A: 1",
-            "B: 3",
-            "C: 5",
-            "D: Der Code ist fehlerhaft"
-        ],
-        "korrekte_antwort": ["C"]
-    },
-    {
-        "frage": "Was ist die erwartete Ausgabe des folgenden Snippets?",
-        "optionen": [
-            "A: 0 1 2 3",
-            "B: range(4)",
-            "C: <generator object fun at (einige Hex-Zahlen)>",
-            "D: Ein Laufzeitfehler tritt auf"
-        ],
-        "korrekte_antwort": ["C"]
+        "korrekte_antwort": "B"
     }
-
-
 ];
 
 let gesamteFragen = [];
@@ -198,25 +106,6 @@ let aktuelleFrageIndex = 0;
 let score = 0;
 let falscheAntworten = [];
 
-document.addEventListener('DOMContentLoaded', function() {
-    loadFragen();
-});
-
-function loadFragen() {
-    fetch('questions.json')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Netzwerkantwort war nicht ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            fragen = data;
-            startQuiz();
-            addEventListeners();
-        })
-        .catch(error => console.error('Fehler beim Laden der Fragen:', error));
-}
 
 function addEventListeners() {
     document.getElementById('nextQuestion').addEventListener('click', checkAnswer);
@@ -258,11 +147,14 @@ function loadQuestion() {
 
     if (questionTextElement && optionsContainer && aktuelleFrageIndex < fragen.length) {
         const aktuelleFrage = fragen[aktuelleFrageIndex];
-        questionTextElement.textContent = aktuelleFrage.frage;
+
+        // Ersetzen Sie Zeilenumbrüche durch <br> Tags und nutzen Sie das <pre>-Tag für die Formatierung
+        questionTextElement.innerHTML = `<pre>${aktuelleFrage.frage.replace(/\n/g, '<br>')}</pre>`;
+
         optionsContainer.innerHTML = ''; // Löschen Sie vorherige Optionen
 
         aktuelleFrage.optionen.forEach((option, index) => {
-            const inputType = aktuelleFrage.korrekte_antwort.length > 1 ? 'checkbox' : 'radio';
+            const inputType = typeof aktuelleFrage.korrekte_antwort === 'string' ? 'radio' : 'checkbox';
             const inputId = `option-${index}`;
             optionsContainer.innerHTML += `
                 <div class="form-check">
@@ -276,6 +168,7 @@ function loadQuestion() {
         endQuiz();
     }
 }
+
 
 function endQuiz() {
     let quizDiv = document.getElementById('quiz');
