88c2d4a0cf0d06531f6652e4d835b6982cac4613
Ben Zen <benediktsplintergfx@gmail.com>
2023-11-24 13:34:01 +0100

    Added questions

--- quiz.js
+++ quiz.js
@@ -38,14 +38,194 @@ const fragen = [
             "D: nein, das ist nicht möglich; ein Modul kann importiert, aber nicht ausgeführt werden"
         ],
         "korrekte_antwort": "A"
+    },
+    {
+        "frage": "Wählen Sie die gültigen fun() Aufrufe aus: (Wählen Sie zwei Antworten):",
+        "optionen": [
+            "A: fun(b = 1)",
+            "B: fun(a = 0)",
+            "C: fun(b = 1 0)",
+            "D: fun(1)"
+        ],
+        "korrekte_antwort": ["B", "D"]
+    },
+    {
+        "frage": "Was sagt ein Dateiname wie dieser unten über die Datei aus? services.cpython-36.pyc (Wählen Sie drei Antworten):",
+        "optionen": [
+            "A: der Interpreter, der zum Generieren der Datei verwendet wurde, ist Version 3.6",
+            "B: sie wurde von CPython produziert",
+            "C: es ist die 36. Version der Datei",
+            "D: die Datei stammt aus der Quelldatei services.py"
+        ],
+        "korrekte_antwort": ["A", "B", "D"]
+    },
+    {
+        "frage": "Was ist das erwartete Verhalten des folgenden Snippets?",
+        "optionen": [
+            "A: verursacht eine Ausnahme",
+            "B: druckt 1",
+            "C: druckt 0 [1]",
+            "D: druckt [1]"
+        ],
+        "korrekte_antwort": ["A"]
+    },
+    {
+        "frage": "Was können Sie tun, wenn Sie einen langen Paketpfad wie diesen nicht mögen?",
+        "optionen": [
+            "A: Sie können einen Alias für den Namen mit dem Schlüsselwort alias erstellen",
+            "B: nichts, Sie müssen sich damit abfinden",
+            "C: Sie können ihn auf alpha.zeta verkürzen und Python wird die richtige Verbindung finden",
+            "D: Sie können einen Alias für den Namen mit dem Schlüsselwort as erstellen"
+        ],
+        "korrekte_antwort": ["D"]
+    },
+    {
+        "frage": "Was ist die erwartete Ausgabe des folgenden Codes?",
+        "optionen": [
+            "A: abcef",
+            "B: Das Programm verursacht eine Laufzeit-Ausnahme/Fehler",
+            "C: acdef",
+            "D: abdef"
+        ],
+        "korrekte_antwort": ["B"]
+    },
+    {
+        "frage": "Welche der folgenden Ausdrücke werden True bewerten? (Wählen Sie zwei Antworten):",
+        "optionen": [
+            "A: 99 > '99'",
+            "B: 'ABc' > 'CAB'",
+            "C: len( \"\"\"\") <= 1",
+            "D: 'True' != True"
+        ],
+        "korrekte_antwort": ["C", "D"]
+    },
+    {
+        "frage": "Welches ist die Ausgabe des folgenden Snippets?",
+        "optionen": [
+            "A: Es wird 1 ausgegeben",
+            "B: Es wird 2 ausgegeben",
+            "C: Es wird 3 ausgegeben",
+            "D: Es tritt ein Laufzeitfehler auf"
+        ],
+        "korrekte_antwort": ["A"]
+    },
+    {
+        "frage": "Welche der folgenden Aussagen über objektorientierte Programmierung (OOP) in Python sind wahr? (Wählen Sie zwei Antworten):",
+        "optionen": [
+            "A: Klassenvariablen werden nicht in einem Objekt's __dict__ angezeigt",
+            "B: Beim Verwenden der hasattr-Methode muss der Name der Eigenschaft eine Klasse oder ein Objekt sein",
+            "C: Python erreicht die Kapselung privater Variablen durch Namensmangling",
+            "D: Eine einzelne Vererbung ist immer schwerer zu pflegen als eine multiple Vererbung"
+        ],
+        "korrekte_antwort": ["A", "C"]
+    },
+    {
+        "frage": "Welches ist die Ausgabe des folgenden Snippets?",
+        "optionen": [
+            "A: [3 2]",
+            "B: [1 2]",
+            "C: 1",
+            "D: 3"
+        ],
+        "korrekte_antwort": ["C"]
+    },
+    {
+        "frage": "Welche der folgenden Code-Snippets würden Sie einfügen, um Klasse B den Konstruktor von Klasse A aufrufen zu lassen? (Wählen Sie zwei Antworten):",
+        "optionen": [
+            "A: super().__init__()",
+            "B: super().__init__(self)",
+            "C: A.__init__()",
+            "D: A.__init__(self)"
+        ],
+        "korrekte_antwort": ["A", "D"]
+    },
+    {
+        "frage": "Welches ist die Ausgabe des folgenden Snippets?",
+        "optionen": [
+            "A: False True False",
+            "B: True False False",
+            "C: True True False",
+            "D: True True True"
+        ],
+        "korrekte_antwort": ["C"]
+    },
+    {
+        "frage": "Was ist wahr über Python-Klassenkonstruktoren? (Wählen Sie zwei Antworten):",
+        "optionen": [
+            "A: Übergeordnete Konstruktoren werden implizit während der Konstruktor-Ausführung aufgerufen",
+            "B: Der Konstruktor kann direkt aufgerufen werden",
+            "C: Der erste Parameter jeder Methode wird verwendet, um auf die Eigenschaften des Objekts zuzugreifen oder seine Methoden aufzurufen",
+            "D: Der Konstruktor kann nichts zurückgeben"
+        ],
+        "korrekte_antwort": ["A", "D"]
+    },
+    {
+        "frage": "Welches ist die Ausgabe des folgenden Snippets?",
+        "optionen": [
+            "A: 1",
+            "B: 2",
+            "C: 3",
+            "D: Der Code ist fehlerhaft"
+        ],
+        "korrekte_antwort": ["C"]
+    },
+    {
+        "frage": "Welches ist die Ausgabe des folgenden Snippets?",
+        "optionen": [
+            "A: 1",
+            "B: 3",
+            "C: 5",
+            "D: Der Code ist fehlerhaft"
+        ],
+        "korrekte_antwort": ["C"]
+    },
+    {
+        "frage": "Was ist die erwartete Ausgabe des folgenden Snippets?",
+        "optionen": [
+            "A: 0 1 2 3",
+            "B: range(4)",
+            "C: <generator object fun at (einige Hex-Zahlen)>",
+            "D: Ein Laufzeitfehler tritt auf"
+        ],
+        "korrekte_antwort": ["C"]
     }
-    // Weitere Fragen...
+
+
 ];
 
+let gesamteFragen = [];
 let aktuelleFrageIndex = 0;
 let score = 0;
-let falscheAntworten = []; // Array zum Speichern der falsch beantworteten Fragen
+let falscheAntworten = [];
 
+document.addEventListener('DOMContentLoaded', function() {
+    loadFragen();
+});
+
+function loadFragen() {
+    fetch('questions.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Netzwerkantwort war nicht ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            fragen = data;
+            startQuiz();
+            addEventListeners();
+        })
+        .catch(error => console.error('Fehler beim Laden der Fragen:', error));
+}
+
+function addEventListeners() {
+    document.getElementById('nextQuestion').addEventListener('click', checkAnswer);
+    let restartQuizButton = document.getElementById('restart-quiz');
+    if (restartQuizButton) {
+        restartQuizButton.addEventListener('click', restartQuiz);
+    }
+    loadQuestion();
+}
 function checkAnswer() {
     let gewaehlteOptionen = document.querySelectorAll('input[name="option"]:checked');
     let gewaehlteAntworten = Array.from(gewaehlteOptionen).map(opt => opt.value);
@@ -68,18 +248,8 @@ function checkAnswer() {
 }
 
 function restartQuiz() {
-    aktuelleFrageIndex = 0;
-    score = 0;
-    falscheAntworten = []; // Zurücksetzen der falsch beantworteten Fragen
-    loadQuestion();
-
-    // Den "Nächste Frage"-Button wieder anzeigen
-    let nextQuestionButton = document.getElementById('nextQuestion');
-    nextQuestionButton.style.display = 'block';
-
-    // Den "Neu starten" Button ausblenden
-    let restartQuizButton = document.getElementById('restart-quiz');
-    restartQuizButton.style.display = 'none';
+    // Die Seite neu laden
+    window.location.reload();
 }
 
 function loadQuestion() {
@@ -155,9 +325,10 @@ function endQuiz() {
 
 document.getElementById('nextQuestion').addEventListener('click', checkAnswer);
 
-// Den "Neu starten" Button ausblenden
-let restartQuizButton = document.getElementById('restart-quiz');
-restartQuizButton.style.display = 'none';
-
-// Laden der ersten Frage beim Start
-loadQuestion();
+document.addEventListener('DOMContentLoaded', function() {
+    let restartQuizButton = document.getElementById('restart-quiz');
+    if (restartQuizButton) {
+        restartQuizButton.addEventListener('click', restartQuiz);
+    }
+    loadQuestion(); // Laden der ersten Frage beim Start
+});
